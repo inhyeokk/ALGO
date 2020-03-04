e52497eee8f90cef9c555660909a904c422784f0
Inhyeok Kang <rkddlsgur983@naver.com>
2020-03-04 16:51:03 +0900

    feat: #2 Solve 0304 Java A+ 대비 SWEA D4 내전경기

--- SWEA/D4/SWEA_7988_D4_내전경기.java
+++ SWEA/D4/SWEA_7988_D4_내전경기.java
@@ -2,10 +2,9 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class SWEA_7988_D4_내전경기 {
@@ -20,9 +19,9 @@ public class SWEA_7988_D4_내전경기 {
 			int k = Integer.parseInt(bf.readLine());
 			
 			/* 몇 명이 입력되는지 모르기때문에
-			 * 입력을 input배열에 저장하면서
-			 * HashMap에 입력되는 input과 해당 인덱스를 매핑
-			 */
+             * 입력을 input배열에 저장하면서
+             * HashMap에 입력되는 input과 해당 인덱스를 매핑
+             */
 			HashMap<String, Integer> map = new HashMap<>();
 			for (int j = 0; j < k; ++j) {
 				st = new StringTokenizer(bf.readLine(), " ");
@@ -37,45 +36,56 @@ public class SWEA_7988_D4_내전경기 {
 				input[j][0] = a;
 				input[j][1] = b;
 			}
-			int size = map.size();
-			Set<Integer>[] graph = new HashSet[size];
-			for (int j = 0; j < size; ++j) {
-				graph[j] = new HashSet<>();
-			}
 			
 			/* 시너지 관계를 저장하기 위한 인접 그래프에
-			 * input에 해당하는 인덱스로 추가
-			 */
+             * input에 해당하는 인덱스로 추가
+             */
+			int size = map.size();
+			List<Integer>[] graph = new LinkedList[size];
 			for (int j = 0; j < k; ++j) {
 				int a = map.get(input[j][0]);
 				int b = map.get(input[j][1]);
+				if (graph[a] == null) {
+					graph[a] = new LinkedList<>();
+				}
+				if (graph[b] == null) {
+					graph[b] = new LinkedList<>();
+				}
 				graph[a].add(b);
 				graph[b].add(a);
 			}
 			
 			/* 시너지 효과를 내지 않고 
-			 * 2개의 팀으로 분리할 수 있는지 확인
-			 */
-			boolean[] visit = new boolean[size];
-			int team = 0;
+             * 2개의 팀으로 분리할 수 있는지 확인
+             */
+			int[] team = new int[size];
+			boolean possible = true;
 			for (int j = 0; j < size; ++j) {
-				if (graph[j].size() == size-1) continue;
-				if (!visit[j]) {
-					visit[j] = true;
+				if (team[j] == 0) {
+					team[j] = 1;
 					queue.add(j);
-					++team;
 				}
-				while (!queue.isEmpty()) {
+				while(!queue.isEmpty()) {
 					int idx = queue.poll();
 					for (Integer l: graph[idx]) {
-						if (!visit[l]) {
-							visit[l] = true;
+						if (team[l] == 0) {
+							if (team[idx] == 1) {
+								team[l] = 2;
+							} else {
+								team[l] = 1;
+							}
+							queue.add(l);
+						} else if (team[l] == team[idx]){
+							possible = false;
+							queue.clear();
+							break;
 						}
 					}
 				}
+				if(!possible) break;
 			}
 			sb.append("#").append(i).append(" ");
-			if (team == 2) {
+			if (possible) {
 				sb.append("Yes");
 			} else {
 				sb.append("No");

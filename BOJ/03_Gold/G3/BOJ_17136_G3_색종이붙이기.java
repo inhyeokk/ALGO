ba53525c86e274572bd4e9c488bd74e2802dd5a2
Inhyeok Kang <rkddlsgur983@naver.com>
2020-03-10 11:02:15 +0900

    feat: BOJ G3 색종이 붙이기 효율 개선

--- BOJ/03_Gold/G3/BOJ_17136_G3_색종이붙이기.java
+++ BOJ/03_Gold/G3/BOJ_17136_G3_색종이붙이기.java
@@ -8,8 +8,8 @@ import java.util.StringTokenizer;
 /**
  * @date   2020-03-10
  * @author rkddlsgur983
- * @memory 278116KB / 512MB
- * @time   460ms / 1초
+ * @memory 20284KB / 512MB
+ * @time   312ms / 1초
  */
 public class BOJ_17136_G3_색종이붙이기 {
 	private static int n = 10;
@@ -26,33 +26,30 @@ public class BOJ_17136_G3_색종이붙이기 {
 				map[i][j] = Integer.parseInt(st.nextToken()) == 1;
 			}
 		}
+		bf.close();
+		
 		for (int i = 0; i < n; ++i) {
 			for (int j = 0; j < n; ++j) {
 				if (!map[i][j]) continue;
 				int range = 1;
-				// 최대 크기 5
+				here: // 최대 크기 5, i,j 위치에서 붙일 수 있는 가장 큰 색종이를 구함
 				while (range < 5 && i+range < n && j+range < n) {
-					boolean possible = true;
 					for (int k = i; k <= i+range; ++k) {
 						if (!map[k][j+range]) {
-							possible = false;
-							break;
+							break here;
 						}
 					}
-					if (!possible) break;
 					for (int k = j; k <= j+range-1; ++k) {
 						if (!map[i+range][k]) {
-							possible = false;
-							break;
+							break here;
 						}
 					}
-					if (!possible) break;
 					++range;
 				}
-				// i,j 위치에서 붙일 수 있는 가장 큰 색종이
 				list.add(new Paper(i,j,range));
 			}
 		}
+		
 		size = list.size();
 		dfs(0, 0, new int[5]);
 		if (min == 26) {
@@ -60,7 +57,6 @@ public class BOJ_17136_G3_색종이붙이기 {
 		} else {
 			System.out.print(min);
 		}
-		bf.close();
 	}
 	
 	private static void dfs(int depth, int total, int[] cnt) {
@@ -79,37 +75,34 @@ public class BOJ_17136_G3_색종이붙이기 {
 			return;
 		}
 		
-		boolean[][] tmap = new boolean[n][n];
-		for (int i = 0; i < n; ++i) {
-			tmap[i] = map[i].clone();
-		}
-		
-		// si크기의 색종이를 현재 위치에 붙일 수 있는지 확인
-		for (int si = p.s; si >= 1; --si) {
-			if (cnt[si-1] == 5) continue; // 각 색종이 당 5개를 넘을 수 없음
+		for (int s = p.s; s >= 1; --s) {
+			if (cnt[s-1] == 5) continue; // 각 색종이 당 5개를 넘을 수 없음
 			boolean possible = true;
-			for (int i = 0; i < si; ++i) {
-				for (int j = 0; j < si; ++j) {
+			here: // s크기의 색종이를 현재 위치에 붙일 수 있는지 확인
+			for (int i = 0; i < s; ++i) {
+				for (int j = 0; j < s; ++j) {
 					if (!map[p.r+i][p.c+j]) {
 						possible = false;
-						break;
+						break here;
 					}
 				}
-				if (!possible) break;
 			}
 			if (!possible) continue;
-			for (int i = 0; i < si; ++i) {
-				for (int j = 0; j < si; ++j) {
-					if (map[p.r+i][p.c+j]) {
-						map[p.r+i][p.c+j] = false;
-					}
+			
+			// s크기의 색종이를 붙임
+			for (int i = 0; i < s; ++i) {
+				for (int j = 0; j < s; ++j) {
+					map[p.r+i][p.c+j] = false;
 				}
 			}
-			++cnt[si-1];
+			++cnt[s-1];
 			dfs(depth+1, total+1, cnt);
-			--cnt[si-1];
-			for (int i = 0; i < n; ++i) {
-				map[i] = tmap[i].clone();
+			--cnt[s-1];
+			// 떼어냄
+			for (int i = 0; i < s; ++i) {
+				for (int j = 0; j < s; ++j) {
+					map[p.r+i][p.c+j] = true;
+				}
 			}
 		}
 	}

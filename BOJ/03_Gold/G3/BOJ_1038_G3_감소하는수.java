53ce5bd9015e17c352a43e599c5e01a56fa2870d
Inhyeok Kang <rkddlsgur983@naver.com>
2020-03-12 00:10:35 +0900

    feat: Solve BOJ G3 감소하는 수

---
+++ BOJ/03_Gold/G3/BOJ_1038_G3_감소하는수.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * @{link} https://www.acmicpc.net/problem/1038
+ * @date   2020-03-12
+ * @author rkddlsgur983
+ * @memory 14268KB / 512MB
+ * @time   112ms / 1초
+ */
+public class BOJ_1038_G3_감소하는수 {
+	private static int n, cnt, len = 1;
+	private static boolean find = false;
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		sc.close();
+		if (n >= 1023) {
+			System.out.println(-1);
+			return;
+		}
+		while (cnt-1 < n) {
+			for (long i = 0L; i < 10; ++i)
+				dfs(1, i, i);
+			++len;
+		}
+	}
+	
+	private static void dfs(int depth, long start, long num) {
+		
+		if (find) return;
+		if (depth == len) {
+			if (cnt++ == n) {
+				find = true;
+				System.out.print(num);
+			}
+			return;
+		}
+		
+		for (long i = 0L; i < start; ++i) {
+			if (find) return;
+			dfs(depth+1, i, num*10L+i);
+		}
+	}
+}

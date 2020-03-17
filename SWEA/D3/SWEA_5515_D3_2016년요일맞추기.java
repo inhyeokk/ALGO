321839ef2541d550285502527770ba46ee8f1e87
Inhyeok Kang <rkddlsgur983@gmail.com>
2020-03-17 23:57:27 +0900

    feat: Solve BOJ G4 숨바꼭질4 등 4문제

---
+++ SWEA/D3/SWEA_5515_D3_2016년요일맞추기.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @date   	2020-03-17
+ * @author 	rkddlsgur983
+ * @memory 	22592KB / 256MB
+ * @time   	110ms / 2초
+ * @idea	단순 계산
+ */
+public class SWEA_5515_D3_2016년요일맞추기 {
+	private static int[] day = {31,29,31,30,31,30,31,31,30,31,30,31};
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int tc = Integer.parseInt(bf.readLine());
+		for (int t = 1; t <= tc; ++t) {
+			st = new StringTokenizer(bf.readLine());
+			int m = Integer.parseInt(st.nextToken());
+			int d = Integer.parseInt(st.nextToken());
+			int ans = 3+d;
+			for (int i = 0; i < m-1; ++i) {
+				ans += day[i];
+			}
+			sb.append("#").append(t).append(" ").append(ans%7).append("\n");
+		}
+		System.out.print(sb);
+		bf.close();
+	}
+}
--- SWEA/D4/SWEA_5604_D4_구간합.java
+++ SWEA/D4/SWEA_5604_D4_구간합.java
@@ -26,41 +26,30 @@ public class SWEA_5604_D4_구간합 {
 			long a = Long.parseLong(st.nextToken());
 			long b = Long.parseLong(st.nextToken());
 			long ans = 0L;
-			int len = b == 0 ? 1 : (int)Math.log10(b)+1;
-			while (len > 1) {
-				long divide = (long)Math.pow(10, len-1);
-				long end = b / divide;
-				ans += len > 1 ? (long)(45*(len-1)*Math.pow(10, len-2)*end) : 0;
-				for (long i = 1L; i < end; ++i) {
-					ans += i * divide; // 35000일때 29999까지 구간합
-				}
-				b %= divide; // 5000
-				ans += end * (b+1);
-				--len;
-			}
-			for (long i = 1L; i <= b; ++i) {
-				ans += i;
-			}
-			
-			--a;
-			len = a == 0 ? 1 : (int)Math.log10(a)+1;
-			while (len > 1) {
-				long divide = (long)Math.pow(10, len-1);
-				long end = a / divide;
-				ans -= len > 1 ? (long)(45*(len-1)*Math.pow(10, len-2)*end) : 0;
-				for (long i = 1L; i < end; ++i) {
-					ans -= i * divide;
-				}
-				a %= divide;
-				ans -= end * (a+1);
-				--len;
-			}
-			for (long i = 1L; i <= a; ++i) {
-				ans -= i;
-			}
+			ans += getSum(b) - getSum(a-1);
 			sb.append("#").append(tc).append(" ").append(ans).append("\n");
 		}
 		System.out.print(sb);
 		bf.close();
+	}
+	
+	private static long getSum(long n) {
+		long sum = 0L;
+		int len = n == 0 ? 1 : (int)Math.log10(n)+1;
+		while (len > 1) {
+			long divide = (long)Math.pow(10, len-1);
+			long end = n / divide;
+			sum += len > 1 ? (long)(45*(len-1)*Math.pow(10, len-2)*end) : 0;
+			for (long i = 1L; i < end; ++i) {
+				sum += i * divide; // 35000일때 29999까지 구간합
+			}
+			n %= divide; // 5000
+			sum += end * (n+1);
+			--len;
+		}
+		for (long i = 1L; i <= n; ++i) {
+			sum += i;
+		}
+		return sum;
 	}
 }

981c24aa437a16da2a50ad6deb5769e7dbfbb040
Inhyeok Kang <rkddlsgur983@gmail.com>
2020-04-02 14:43:20 +0900

    feat: Solve PRO L4 호텔 방 배정

---
+++ Programmers/코딩테스트 연습/2019 카카오 개발자 겨울 인턴십/PRO_L4_호텔방배정.java
@@ -0,0 +1,35 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @{link}	https://programmers.co.kr/learn/courses/30/lessons/64063
+ * @date   	2020-04-02
+ * @author 	rkddlsgur983
+ * @idea	union-find 
+ * 			key가 없거나 현재와 같으면 다음 위치의 key와 union
+ */
+public class PRO_L4_호텔방배정 {
+	private Map<Long, Long> map = new HashMap<>();
+	public long[] solution(long k, long[] room_number) {
+		int len = room_number.length;
+		long[] answer = new long[len];
+		for (int i = 0; i < len; ++i) {
+			if (!map.containsKey(room_number[i]) || map.get(room_number[i]) == room_number[i]) {
+				answer[i] = room_number[i];
+				map.put(room_number[i], find(room_number[i]+1));
+			} else {
+				answer[i] = find(room_number[i]);
+				map.put(answer[i], find(answer[i]+1));
+			}
+		}
+        return answer;
+    }
+	
+	private long find(long x) {
+		if (!map.containsKey(x) || map.get(x) == x) {
+			return x;
+		}
+		map.put(x, find(map.get(x)));
+		return map.get(x);
+	}
+}

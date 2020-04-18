2a6462e7944b89ce03a3c6f60f58c944eb07c24d
Inhyeok Kang <rkddlsgur983@naver.com>
2020-04-18 18:10:03 +0900

    feat: BOJ G4 거짓말 주석 추가

--- src/boj/BOJ_G4_1043_거짓말.java
+++ src/boj/BOJ_G4_1043_거짓말.java
@@ -29,7 +29,7 @@ public class BOJ_G4_1043_거짓말 {
 		for (int i = 0; i < k; ++i) {
 			int w = Integer.parseInt(st.nextToken())-1;
 			visit[w] = true;
-			queue.add(w);
+			queue.add(w); // 진실을 알고 있는 사람
 		}
 		int[][] party = new int[m][];
 		boolean[] pvisit = new boolean[m];
@@ -43,7 +43,7 @@ public class BOJ_G4_1043_거짓말 {
 			party[i] = new int[s];
 			for (int j = 0; j < s; ++j) {
 				party[i][j] = Integer.parseInt(st.nextToken())-1;
-				graph[party[i][j]].add(i);
+				graph[party[i][j]].add(i); // 각 사람이 어떤 파티에 포함되었는지 추가
 			}
 		}
 		br.close();
@@ -51,10 +51,13 @@ public class BOJ_G4_1043_거짓말 {
 		while (!queue.isEmpty()) {
 			int i = queue.poll();
 			while (!graph[i].isEmpty()) {
-				int j = graph[i].remove(0);
+				int j = graph[i].remove(0); // 진실을 알고 있는 사람이 속한 파티
 				if (!pvisit[j]) {
-					pvisit[j] = true;
+					pvisit[j] = true; // 이 파티에서는 진실을 이야기 해야함
 					--m;
+					/* 이 파티에 오는 사람들 또한 진실을 알게되므로
+					 * 진실을 알고 있는 사람을 담는 큐에 추가
+					 */
 					for (int l = 0, len = party[j].length; l < len; ++l) {
 						if (!visit[party[j][l]]) {
 							visit[party[j][l]] = true;

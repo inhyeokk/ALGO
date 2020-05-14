cc7f43b379730f28485d4451264f67bc8f5a4487
Inhyeok Kang <rkddlsgur983@naver.com>
2020-05-14 15:38:33 +0900

    feat: BOJ G1 달이 차오른다 가자 로직 개선

--- src/boj/BOJ_G1_1194_달이차오른다가자.java
+++ src/boj/BOJ_G1_1194_달이차오른다가자.java
@@ -12,9 +12,11 @@ import java.util.StringTokenizer;
  * @{link}	https://www.acmicpc.net/problem/1194
  * @date   	2020-05-13
  * @author 	rkddlsgur983
- * @memory 	19188KB / 128MB
- * @time   	132ms / 2초
+ * @memory 	15644KB / 128MB
+ * @time   	108ms / 2초
  * @idea	비트마스킹, BFS
+ * 			도착지점을 최초로 방문 했을 때가 최소 거리이다.
+ * 			가중치 체크가 아닌 방문 체크만으로 가능
  */
 public class BOJ_G1_1194_달이차오른다가자 {
 	private static int[][] di = {{0,1},{1,0},{0,-1},{-1,0}};
@@ -25,7 +27,6 @@ public class BOJ_G1_1194_달이차오른다가자 {
 		int m = Integer.parseInt(st.nextToken());
 		char[][] map = new char[n][m];
 		int sr = 0, sc = 0;
-		List<int[]> exit = new LinkedList<>();
 		for (int i = 0; i < n; ++i) {
 			String s = br.readLine();
 			for (int j = 0; j < m; ++j) {
@@ -35,16 +36,14 @@ public class BOJ_G1_1194_달이차오른다가자 {
 					sc = j;
 					map[i][j] = '.';
 				}
-				if (map[i][j] == '1') {
-					exit.add(new int[] {i,j});
-				}
 			}
 		}
 		br.close();
-		int[][][] weight = new int[n][m][64];
+		
+		boolean[][][] visit = new boolean[n][m][64];
 		Queue<Move> q = new LinkedList<>();
-		q.add(new Move(sr,sc,0));
-		weight[sr][sc][0] = 1;
+		q.add(new Move(sr,sc,0,0));
+		visit[sr][sc][0] = true;
 		while (!q.isEmpty()) {
 			Move o = q.poll();
 			for (int[] d: di) {
@@ -52,50 +51,42 @@ public class BOJ_G1_1194_달이차오른다가자 {
 				int nc = o.c + d[1];
 				if (nr >= 0 && nr < n && nc >= 0 && nc < m && map[nr][nc] != '#') {
 					if (map[nr][nc] == '1') {
-						if (weight[nr][nc][o.k] == 0 || weight[nr][nc][o.k] > weight[o.r][o.c][o.k]+1) {
-							weight[nr][nc][o.k] = weight[o.r][o.c][o.k]+1;
-						}
+						System.out.print(o.v+1);
+						return;
 					} else if (map[nr][nc] == '.') {
-						if (weight[nr][nc][o.k] == 0 || weight[nr][nc][o.k] > weight[o.r][o.c][o.k]+1) {
-							weight[nr][nc][o.k] = weight[o.r][o.c][o.k]+1;
-							q.add(new Move(nr,nc,o.k));
+						if (!visit[nr][nc][o.k]) {
+							visit[nr][nc][o.k] = true;
+							q.add(new Move(nr,nc,o.k,o.v+1));
 						}
 					} else if (map[nr][nc] >= 'a' && map[nr][nc] <= 'f') {
 						int tk = o.k | 1 << (map[nr][nc]-'a');
-						if (weight[nr][nc][tk] == 0 || weight[nr][nc][tk] > weight[o.r][o.c][o.k]+1) {
-							weight[nr][nc][tk] = weight[o.r][o.c][o.k]+1;
-							q.add(new Move(nr,nc,tk));
+						if (!visit[nr][nc][tk]) {
+							visit[nr][nc][tk] = true;
+							q.add(new Move(nr,nc,tk,o.v+1));
 						}
 					} else {
 						if ((o.k & 1 << (map[nr][nc]-'A')) <= 0) continue;
-						if (weight[nr][nc][o.k] == 0 || weight[nr][nc][o.k] > weight[o.r][o.c][o.k]+1) {
-							weight[nr][nc][o.k] = weight[o.r][o.c][o.k]+1;
-							q.add(new Move(nr,nc,o.k));
+						if (!visit[nr][nc][o.k]) {
+							visit[nr][nc][o.k] = true;
+							q.add(new Move(nr,nc,o.k,o.v+1));
 						}
 					}
 				}
 			}
 		}
-		int min = Integer.MAX_VALUE;
-		for (int i = 0, size = exit.size(); i < size; ++i) {
-			int r = exit.get(i)[0];
-			int c = exit.get(i)[1];
-			for (int j = 0; j < 64; ++j) {
-				if (weight[r][c][j] == 0) continue;
-				min = Math.min(weight[r][c][j], min);
-			}
-		}
-		System.out.print(min == Integer.MAX_VALUE ? -1 : min-1);
+		System.out.print(-1);
 	}
 	
 	static class Move {
 		int r;
 		int c;
 		int k;
-		public Move(int r, int c, int k) {
+		int v;
+		public Move(int r, int c, int k, int v) {
 			this.r = r;
 			this.c = c;
 			this.k = k;
+			this.v = v;
 		}
 	}
 }

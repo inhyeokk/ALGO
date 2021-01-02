d8e37ba8b46b9f49fb55270f775e668245aa6a28
Inhyeok Kang <rkddlsgur983@daum.net>
2021-01-03 03:19:18 +0900

    ALGO-10 백준 특정한 최단경로 풀이 개선

--- src/boj/BOJ_1504.java
+++ src/boj/BOJ_1504.java
@@ -10,23 +10,20 @@ import java.util.StringTokenizer;
 
 /**
  * {@link}  https://www.acmicpc.net/problem/1504
- * @date    2021-01-01
+ * @date    2021-01-03
  * @author  lolol0705
- * @memory  74212KB / 256MB
- * @time    896ms / 1초
+ * @memory  70456KB / 256MB
+ * @time    600ms / 1초
  */
 public class BOJ_1504 {
     private static final int MAX = 2000;
-    private static int n, e;
-    private static List<Node>[] graph;
-    private static int v1, v2;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        n = Integer.parseInt(st.nextToken());
-        e = Integer.parseInt(st.nextToken());
-        graph = new List[n+1];
+        int n = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        List<Edge>[] graph = new List[n + 1];
         for (int i = 1; i <= n; ++i) {
             graph[i] = new LinkedList<>();
         }
@@ -35,85 +32,65 @@ public class BOJ_1504 {
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            graph[a].add(new Node(b, c)); // 1
-            graph[b].add(new Node(a, c));
+            graph[a].add(new Edge(b, c)); // 1
+            graph[b].add(new Edge(a, c));
         }
         st = new StringTokenizer(br.readLine(), " ");
-        v1 = Integer.parseInt(st.nextToken());
-        v2 = Integer.parseInt(st.nextToken());
+        int v1 = Integer.parseInt(st.nextToken());
+        int v2 = Integer.parseInt(st.nextToken());
         br.close();
 
-        int first = first(); // 2
-        int second = second(); // 3
-        System.out.print(Math.min(first, second)); // 4
+        Edge[] v1_edges = dijkstra(graph, n, v1);
+        Edge[] v2_edges = dijkstra(graph, n, v2);
+
+        if (v1_edges[1].v == MAX || v1_edges[v2].v == MAX || v2_edges[n].v == MAX
+                || v2_edges[1].v == MAX || v2_edges[v1].v == MAX || v1_edges[n].v == MAX) {
+            System.out.print(-1);
+        } else {
+            // 1 -> v1 -> v2 -> N
+            int first = v1_edges[1].v + v1_edges[v2].v + v2_edges[n].v; // 2
+            // 1 -> v2 -> v1 -> N
+            int second = v2_edges[1].v + v2_edges[v1].v + v1_edges[n].v; // 3
+            System.out.print(Math.min(first, second)); // 4
+        }
     }
 
-    // 1 -> v1 -> v2 -> N
-    private static int first() {
-        int dis = dijkstra(1, v1);
-        if (dis == MAX) return -1;
-
-        int temp = dijkstra(v1, v2);
-        if (temp == MAX) return -1;
-        dis += temp;
-
-        temp = dijkstra(v2, n);
-        if (temp == MAX) return -1;
-        dis += temp;
-        return dis;
-    }
-
-    // 1 -> v2 -> v1 -> N
-    private static int second() {
-        int dis = dijkstra(1, v2);
-        if (dis == MAX) return -1;
-
-        int temp = dijkstra(v2, v1);
-        if (temp == MAX) return -1;
-        dis += temp;
-
-        temp = dijkstra(v1, n);
-        if (temp == MAX) return -1;
-        dis += temp;
-        return dis;
-    }
-
-    private static int dijkstra(int start, int end) {
-        Node[] nodes = new Node[n+1];
-        PriorityQueue<Node> pq = new PriorityQueue<>();
+    private static Edge[] dijkstra(List<Edge>[] graph, int n, int start) {
+        Edge[] edges = new Edge[n+1];
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
         for (int i = 1; i <= n; ++i) {
             if (i == start) {
-                nodes[i] = new Node(i, 0);
+                edges[i] = new Edge(i, 0);
             } else {
-                nodes[i] = new Node(i, MAX);
+                edges[i] = new Edge(i, MAX);
             }
-            pq.add(nodes[i]);
+            pq.add(edges[i]);
         }
 
         while (!pq.isEmpty()) {
-            Node node = pq.poll();
-            for (Node other: graph[node.i]) {
-                if (nodes[other.i].v > node.v + other.v) {
-                    nodes[other.i].v = node.v + other.v;
-                    pq.remove(nodes[other.i]);
-                    pq.add(nodes[other.i]);
+            Edge edge = pq.poll();
+            for (Edge other: graph[edge.i]) {
+                if (edges[other.i].v > edge.v + other.v) {
+                    edges[other.i].v = edge.v + other.v;
+                    pq.remove(edges[other.i]);
+                    pq.add(edges[other.i]);
                 }
             }
         }
-        return nodes[end].v;
+        return edges;
     }
 
-    private static class Node implements Comparable<Node> {
+    static class Edge implements Comparable<Edge> {
         int i;
         int v;
 
-        public Node(int i, int v) {
+        public Edge(int i, int v) {
             this.i = i;
             this.v = v;
         }
 
         @Override
-        public int compareTo(Node o) {
+        public int compareTo(Edge o) {
             return Integer.compare(v, o.v);
         }
     }
